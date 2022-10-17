534c940e99261b1e20c5775232ff0bd91f7a53f7
Azer444 <azerrh@code.edu.az>
2022-10-17 21:58:14 +0400

    Fiorello

---
+++ Fiorello/Fiorello/Migrations/20221017150406_CreateFooterTable.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Fiorello.Migrations
+{
+    public partial class CreateFooterTable : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Footers",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    IsDeleted = table.Column<bool>(nullable: false),
+                    Customer = table.Column<int>(nullable: false),
+                    Company = table.Column<int>(nullable: false),
+                    SocialMedia = table.Column<int>(nullable: false),
+                    Archive = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Footers", x => x.Id);
+                });
+
+            migrationBuilder.UpdateData(
+                table: "Blogs",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "Date",
+                value: new DateTime(2022, 10, 17, 19, 4, 6, 100, DateTimeKind.Local).AddTicks(2472));
+
+            migrationBuilder.UpdateData(
+                table: "Blogs",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "Date",
+                value: new DateTime(2022, 10, 17, 19, 4, 6, 102, DateTimeKind.Local).AddTicks(1071));
+
+            migrationBuilder.UpdateData(
+                table: "Blogs",
+                keyColumn: "Id",
+                keyValue: 3,
+                column: "Date",
+                value: new DateTime(2022, 10, 17, 19, 4, 6, 102, DateTimeKind.Local).AddTicks(1131));
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Footers");
+
+            migrationBuilder.UpdateData(
+                table: "Blogs",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "Date",
+                value: new DateTime(2022, 10, 17, 18, 45, 40, 803, DateTimeKind.Local).AddTicks(8750));
+
+            migrationBuilder.UpdateData(
+                table: "Blogs",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "Date",
+                value: new DateTime(2022, 10, 17, 18, 45, 40, 804, DateTimeKind.Local).AddTicks(7727));
+
+            migrationBuilder.UpdateData(
+                table: "Blogs",
+                keyColumn: "Id",
+                keyValue: 3,
+                column: "Date",
+                value: new DateTime(2022, 10, 17, 18, 45, 40, 804, DateTimeKind.Local).AddTicks(7756));
+        }
+    }
+}
